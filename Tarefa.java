f285c7cfcae06746b86df9e9fe07d62a9bf9f33a
diogoBenevenuto <63623546+diogoBenevenuto@users.noreply.github.com>
2020-05-04 19:40:32 -0300

    primeiro exercício

---
+++ Tarefa.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+public class Tarefa {
+
+	public static void main(String[] args) {
+			
+			String nome;
+			String cidade; 
+			int idade;
+			
+			Scanner leitor = new Scanner(System.in);
+			
+			System.out.print("Digite seu nome: ");
+			nome = leitor.nextLine();
+		
+			System.out.println("Bem Vindo");
+			
+			System.out.println("Digite sua idade: ");
+			idade = leitor.nextInt();
+			
+			System.out.println("Meus nome é "+nome+" e eu tenho "+idade+" ano.");
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+	}
+
+}
