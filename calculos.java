b9a138190820d2902379643c7d038c130c94c32c
diogoBenevenuto <63623546+diogoBenevenuto@users.noreply.github.com>
2020-05-05 00:34:15 -0300

    a segunda parte

---
+++ calculos.java
@@ -0,0 +1,26 @@
+
+public class calculos {
+
+	public static void main(String[] args) {
+		
+		int x = 15;
+		int y = 3;
+		
+		int soma = x+y;
+		int subtracao = x-y;
+		int multiplicacao = x*y;
+		int divisao = x/y;
+		int modulo = x%y;
+		
+		System.out.println("soma: " + soma);
+		System.out.println("soma: " + subtracao);
+		System.out.println("soma: " + multiplicacao);
+		System.out.println("soma: " + divisao);
+		System.out.println("soma: " + modulo);
+		
+		
+		
+
+	}
+
+}
